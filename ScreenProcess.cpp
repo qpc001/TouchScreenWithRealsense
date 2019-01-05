f492da2a0d2c339e987e3db4216a87612094f955
qpc001 <406780373@qq.com>
2019-01-05 23:13:34 +0800

    2019-1-4

---
+++ ScreenProcess.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: ScreenProcess.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年01月05日 星期六 22时54分34秒
+ ************************************************************************/
+#include"ScreenProcess.h"
+#include<iostream>
+
+ScreenProcess::ScreenProcess()
+{
+
+    int k=0;
+}
---
+++ ScreenProcess.h
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: ScreenProcess.h
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年01月05日 星期六 22时54分24秒
+ ************************************************************************/
+
+#ifndef _SCREENPROCESS_H
+#define _SCREENPROCESS_H
+#endif
+
+#include<iostream>
+#include<math.h>
+#include"PlaneEquation.h"
+#include"SelfDefPoint.h"
+
+class ScreenProcess 
+{
+    public:
+        ScreenProcess();
+        virtual ~ScreenProcess(){};
+};
